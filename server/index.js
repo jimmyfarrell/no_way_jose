dab45f95e53bcf9fe46476f9d1c2c4c2411605dc
jimmyfarrell <jfarrell22@gmail.com>
2017-02-25 17:29:51 -0500

    Webpack setup + react dependencies to package.json

--- server/index.js
+++ server/index.js
@@ -1,11 +1,27 @@
 const express = require('express');
+const path = require('path');
+const webpack = require('webpack');
 
 const app = express();
+const webpackConfig = require('./webpack.config');
+const compiler = webpack(webpackConfig);
 
-app.get('/', (req, res) => {
-  res.send('There is nothing here...');
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: webpackConfig.output.publicPath
+}));
+
+app.use(require('webpack-hot-middleware')(compiler));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client', 'index.html'));
 });
 
-app.listen(3000, () => {
+app.listen(3000, 'localhost', (err) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
   console.log('Listening on port 3000');
 });
---
+++ server/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    './client'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  ],
+  module: {
+    loaders: [
+    {
+      test: /\.js$/,
+      loaders: ['babel'],
+      include: path.join(__dirname, 'src')
+    }]
+  }
+};
